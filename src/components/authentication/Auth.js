4fea8e661cdffc1cc0d08173da06353111dbab93
Nad1m-A-A <nadim.alaa@hotmail.com>
2023-06-11 19:07:07 +0400

    user auth and contextAPI added

--- src/components/authentication/Auth.js
+++ src/components/authentication/Auth.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import classes from '../../assets/styles/Main.module.css'
 import Signup from './Signup'
 import Login from './Login';
+
 function Auth() {
     const [member, setMember] = useState(false);
     return (
         <div className={classes.auth}>
-            {!member && <Signup/>}
+            {!member && <Signup setMember={setMember}/>}
             {member && <Login/>}
-            <div>{member ? "Not" : "Already"} a member? <span onClick={() => setMember(!member)} className={classes.member_button}>{member ? 'signup' : 'login'}</span></div>
+            <div>
+                {member ?
+                    "Not" :
+                    "Already"}
+                    a member? 
+                <span onClick={() => setMember(!member)} className={classes.member_button}>
+                    {member ?
+                    ' signup' :
+                    ' login'}
+                </span>
+            </div>
 
         </div>
     )
